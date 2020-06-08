7e5ef8f14f6a03e842ac29f8f0f89b104ec26fa5
FrancoValenteG <65205603+FrancoValenteG@users.noreply.github.com>
2020-06-08 20:45:20 -0300

    Update EjercicioN°2.py

--- EjercicioN°2.py
+++ EjercicioN°2.py
@@ -1,22 +1,91 @@
+from configuracion import *
+from principal import *
+import math
 import random
+items=["colores","paises","animales"]
+colores=["rojo","azul","amarillo","negro","blanco","celeste","verde","rosa"]
+paises=["argentina","uruguay","brasil","cuba","venezuela","rusia","rui","albania"]
+animales=["araña","mono","jirafa","gato","perro","jabali","elefante","pez","cocodrilo","rinoceronte","caballo","armadillo"]
+listaDeTodo=[colores,paises,animales]
+lista=["a", "b","c","d","e","f","g","h","i","j","k","l","m","o","p","s","t","u","v","w","x","y","z"]
+palabraUsuario="argentina"
+salida=[]
+lista0=[]
+lista1=[]
+lista2=[]
+def unaAlAzar(lista):
+    return (random.choice(lista))             #Devuelve una letra al azar de la lista.
 
-a = ['1611','1688','2659','1613','1613','1628','1562','1111',"1613","1658"]
-b = ["Lorena","Fernando","Mariana","Máximo","Daniel","Ariel","Cristina","Santiago","Carlos","Iván"]
-c = ['Pérez','Espinosa','Naniz','Fernández','Lancha','Negro','Vholve','Father',"Enrique","Enrique"]
-e = [1,23,11,58,74,6,42,99,28,63]
-lista=[]
-aux=[]
-edad16=[16]
-edad65=[65]
-def listaDNI(a,b,c,e):
-    for d in range(0,10):
-        edadsiono=[]
-        if e <= edad16 or e >= edad65:
-            edadsiono="No apto"
+letra=unaAlAzar(lista)
+letraAzar=letra
+
+for listitas in listaDeTodo:
+        if palabraUsuario in listitas:
+            item=listaDeTodo.index(listitas)
+
+def esCorrecta(palabraUsuario, letra, item, items, listaDeTodo):
+    for i in range(len(items)):
+        if item==items[i]:
+            pos=i
+            print(pos)
+    for j in range(len(palabraUsuario)):
+        if palabraUsuario[0]==letra and palabraUsuario in listaDeTodo[pos]:
+             puntaje= 10
         else:
-            edadsiono="Apto"
-        aux="DNI : "+a[d]+" : "+"\\"+edadsiono+"\\"+" "+c[d]+" "+b[d]
-        lista.append(aux)
-        random.shuffle(lista)
-    return lista
-print(listaDNI(a,b,c,e))
+             puntaje= -10
+    return puntaje
+##print(esCorrecta(palabraUsuario, letra, item, items,listaDeTodo))
+
+
+def juegaCompu(letraAzar,listaDeTodo):
+    for elemento in listaDeTodo[0]:
+            if elemento[0]==letra:
+                lista0.append(elemento)
+            else:
+                salida.append("")
+    for elemento in listaDeTodo[1]:
+            if elemento[0]==letra:
+                lista1.append(elemento)
+            else:
+                salida.append("")
+    for elemento in listaDeTodo[2]:
+            if elemento[0]==letra:
+                lista2.append(elemento)
+            else:
+                salida.append("")
+    azar=random.choice(lista0)
+    azar1=random.choice(lista1)
+    azar2=random.choice(lista2)
+    salida.append(azar)
+    salida.append(azar1)
+    salida.append(azar2)
+    return(salida)
+print(juegaCompu(letraAzar,listaDeTodo))
+
+
+#Funcion que transforma una lista a un string
+def listToString(s):
+    str1 = ""
+    return (str1.join(s))
+def listatotal():
+###Abro el archivo desactualizado y lo paso a una lista
+##    with open('paises.txt', 'r') as f:
+##        listaf=f.readlines()
+###Abro el archivo actualizado y lo paso a una lista
+##    with open('pnuevos.txt', 'r') as p:
+##        listap=p.readlines()
+###Vuelvo a abrir el primer archivo para agregar paises
+##    with open('paises.txt', 'a') as f2:
+##        listaf2=f2.write(listToString(listap))
+###Sin embargo, está todo comentado porque ya se hizo de antemano y no se necesita!
+    f = open("paises.txt","r")
+    paises = [linea.rstrip() for linea in sorted(set(f))]
+    f.close()
+    return(paises)
+
+print(listatotal())
+
+
+
+
+
